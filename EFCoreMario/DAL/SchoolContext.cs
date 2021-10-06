d5f2d980576f3e162b760f1daab943f666a178d8
Mario <Mario@DESKTOP-MARIO>
2021-10-05 23:40:22 -0400

    Add project files.

---
+++ EFCoreMario/DAL/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using EFCoreMario.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreMario.DAL
+{
+    class SchoolContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EFCoreMario/Program.cs
@@ -0,0 +1,55 @@
+﻿using EFCoreMario.DAL;
+using EFCoreMario.Models;
+using System;
+using System.Linq;
+
+namespace EFCoreMario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mario");
+
+            var context = new SchoolContext();
+            /*var studentsWithSameName = context.Students
+                                              .Where(s => s.Name == GetName())
+                                              .ToList();*/
+
+            Guardar(context, "hola");
+            Modificar(context, "Yeuri");
+            Eliminar(context);
+
+        }
+
+        public static string GetName()
+        {
+            return "Mario";
+        }
+
+
+        static public void Guardar(SchoolContext contexto,String name)
+        {
+            var std = new Student()
+            {
+                Name = name,
+            };
+            contexto.Students.Add(std);
+            contexto.SaveChanges();
+        }
+
+        static public void Modificar(SchoolContext contexto, String name)
+        {
+            var std = contexto.Students.First<Student>();
+            std.Name = name;
+            contexto.SaveChanges();
+        }
+
+        static public void Eliminar(SchoolContext contexto)
+        {
+            var std = contexto.Students.First<Student>();
+            contexto.Students.Remove(std);
+            contexto.SaveChanges();
+        }
+    }
+}
